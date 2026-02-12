c5e341e3bfef350a09efb45ceac366972ed0d5e8
sakmoto <hongbonsak2@gmail.com>
2026-02-12 15:54:43 +0700

    comit

--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -11,7 +11,12 @@ function App() {
       <main>
         <section className="places-category">
           <ul className="places">
-            {/* For each place from AVAILABLE_PLACES, create a Place component */}
+            {AVAILABLE_PLACES.map((place) => (
+              <Place 
+                key={place.id} 
+                place={place}
+              />
+            ))}
           </ul>
         </section>
       </main>
